1e74d5d4dcbc12f02d540f1ff7c5aed59a93a983
Artem <zenit222555@mail.ru>
2021-02-01 02:13:32 +0400

    Format document

--- script.js
+++ script.js
@@ -13,33 +13,33 @@ for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
         b = prompt("Во сколько это обойдется?");
     if ((typeof (a) != null) && (typeof (b) != null) && (typeof (a) === 'string') && (typeof (b) === 'string') &&
-        (a != "") && (b != "") && (a.length<50) ) {
-            console.log("All ok!");
+        (a != "") && (b != "") && (a.length < 50)) {
+        console.log("All ok!");
         appData.expenses[a] = b;
-    }else{
+    } else {
         console.log("Bad result");
         i--;
     }
 };
 //let p = 0;
 //while (p < 2){
-  //  for (let i = 0; i < 2; i++) {
-    //    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-      //      b = prompt("Во сколько это обойдется?");
-        //if ((typeof (a) != null) && (typeof (b) != null) && (typeof (a) === 'string') && (typeof (b) === 'string') &&
-          //  (a != "") && (b != "") && (a.length<50) ) {
-            //    console.log("All ok!");
-            //appData.expenses[a] = b;
-       // };
-        // console.log(appData.expenses);
-  //  p++;
+//  for (let i = 0; i < 2; i++) {
+//    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//      b = prompt("Во сколько это обойдется?");
+//if ((typeof (a) != null) && (typeof (b) != null) && (typeof (a) === 'string') && (typeof (b) === 'string') &&
+//  (a != "") && (b != "") && (a.length<50) ) {
+//    console.log("All ok!");
+//appData.expenses[a] = b;
+// };
+// console.log(appData.expenses);
+//  p++;
 //}
-appData.MoneyPerDay=appData.bank/30;
+appData.MoneyPerDay = appData.bank / 30;
 alert("Ваш ежедневный бюджет: " + appData.MoneyPerDay);
-if(appData.MoneyPerDay<300){
+if (appData.MoneyPerDay < 300) {
     console.log("Low level of profit")
-}else if((appData.MoneyPerDay > 300 ) && (appData.MoneyPerDay < 1000)){
+} else if ((appData.MoneyPerDay > 300) && (appData.MoneyPerDay < 1000)) {
     console.log("Midium level of profit")
-}else if(appData.MoneyPerDay>100){
+} else if (appData.MoneyPerDay > 100) {
     console.log("High level of profit")
 }
